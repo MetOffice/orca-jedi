266fd797a24219954d361c10ef6d22b6c071d6b5
Toby Searle <14909402+twsearle@users.noreply.github.com>
2021-12-20 15:44:37 +0000

    fix: Refactor due to OOPS PR#1333 (#4)
    The test failures was due to some missing test data. Thanks!

---
+++ src/orca-jedi/variablechanges/VariableChange.h
@@ -0,0 +1,47 @@
+/*
+ * (C) British Crown Copyright 2021 Met Office
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+*/
+
+#pragma once
+
+#include <memory>
+#include <ostream>
+#include <string>
+
+#include "eckit/config/Configuration.h"
+#include "oops/util/Printable.h"
+#include "orca-jedi/geometry/Geometry.h"
+#include "orca-jedi/state/State.h"
+#include "orca-jedi/variablechanges/VariableChangeParameters.h"
+
+// Forward declarations
+namespace oops {
+  class Variables;
+}
+
+namespace orcamodel {
+
+// -----------------------------------------------------------------------------
+
+class VariableChange: public util::Printable,
+  private util::ObjectCounter<VariableChange> {
+ public:
+  typedef VariableChangeParameters Parameters_;
+  static const std::string classname() {
+    return "orcamodel::VariableChange";
+  }
+  VariableChange(const Parameters_ &, const Geometry &) {}
+  void changeVar(State &, const oops::Variables &) const {}
+  void changeVarInverse(State &, const oops::Variables &) const {}
+
+ private:
+  void print(std::ostream & out) const override {
+    out << "orcamodel::VariableChange: Not Implemented"; };
+};
+
+}  // namespace orcamodel
+
+
