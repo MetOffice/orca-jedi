a2ce02f9ca6cf8346dcc60a246b61455cc80e4e5
twsearle <14909402+twsearle@users.noreply.github.com>
2023-09-27 17:00:55 +0100

    testing output

--- ci/build.sh
+++ ci/build.sh
@@ -41,28 +41,28 @@ ln -s '..' "${HERE}/orca-jedi"
 ecbuild -S "${HERE}"
 make -j "${NPROC}"
 
-# echo ls
-# ls
+ echo ls
+ ls
 
-# echo ls share
-# ls share
+ echo ls share
+ ls share
 
-# echo ls atlas-orca
-# ls atlas-orca
+ echo ls atlas-orca
+ ls atlas-orca
 
-# echo ls atlas
-# ls atlas
+ echo ls atlas
+ ls atlas
 
-# echo ls atlas/share/plugins
-# ls atlas/share/plugins
+ echo ls atlas/share/plugins
+ ls atlas/share/plugins
 
-# echo ls atlas-orca/share/plugins
-# ls atlas-orca/share/plugins
+ echo ls atlas-orca/share/plugins
+ ls atlas-orca/share/plugins
 
-# echo ls share/plugins
-# ls share/plugins
+ echo ls share/plugins
+ ls share/plugins
 
-# exit 1
+ exit 1
 
 env OMPI_ALLOW_RUN_AS_ROOT=1 OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 \
     ATLAS_TRACE=1 ATLAS_DEBUG=1 \
