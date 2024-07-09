e48ccf50055073dec724ceeb03740853f70d90d5
twsearle <14909402+twsearle@users.noreply.github.com>
2024-07-09 13:42:10 +0100

    add hofx3d amm1 test

--- src/tests/orca-jedi/test_interpolator.cc
+++ src/tests/orca-jedi/test_interpolator.cc
@@ -4,6 +4,7 @@
 
 #include<sstream>
 #include<cmath>
+#include <map>
 
 #include "eckit/log/Bytes.h"
 #include "eckit/config/LocalConfiguration.h"
@@ -29,105 +30,182 @@ namespace test {
 
 const double ATOL = 1e-6;
 
+struct InterpTestSettingsFixture {
+ public:
+  eckit::LocalConfiguration geometry_config;
+  eckit::LocalConfiguration state_config;
+  eckit::LocalConfiguration interpolator_config;
+  size_t nlocs, nlevs;
+  std::vector<double> lons;
+  std::vector<double> lats;
+  oops::Variables surf_vars, vol_vars;
+  std::vector<double> surf_values;
+  std::vector<double> vol_values;
+};
+
+const double missing_value = util::missingValue<double>();
+
 //-----------------------------------------------------------------------------
 
-CASE("test basic interpolator") {
-  int nlevs = 3;
-  eckit::LocalConfiguration config;
-  config.set("grid name", "ORCA2_T");
-  config.set("number levels", nlevs);
+CASE("test  interpolator") {
+  std::map<std::string, InterpTestSettingsFixture> settings_map{
+      {"ORCA2_T", InterpTestSettingsFixture()}, {"AMM1", InterpTestSettingsFixture()}};
 
-  std::vector<eckit::LocalConfiguration> nemo_var_mappings(4);
-  nemo_var_mappings[0].set("name", "sea_ice_area_fraction")
-    .set("nemo field name", "iiceconc")
-    .set("model space", "surface");
-  nemo_var_mappings[1].set("name", "sea_ice_area_fraction_error")
-    .set("nemo field name", "sic_tot_var")
-    .set("model space", "surface")
-    .set("variable type", "background variance");
-  nemo_var_mappings[2].set("name", "sea_surface_foundation_temperature")
-    .set("nemo field name", "votemper")
-    .set("model space", "surface");
-  nemo_var_mappings[3].set("name", "sea_water_potential_temperature")
-    .set("nemo field name", "votemper")
-    .set("model space", "volume");
-  config.set("nemo variables", nemo_var_mappings);
-  Geometry geometry(config, eckit::mpi::comm());
+  // ORCA2_T settings
+  {
+    settings_map["ORCA2_T"].nlocs = 3;
+    settings_map["ORCA2_T"].nlevs = 3;
+    settings_map["ORCA2_T"].geometry_config.set("grid name", "ORCA2_T");
+    settings_map["ORCA2_T"].geometry_config.set("number levels", settings_map["ORCA2_T"].nlevs);
 
-  eckit::LocalConfiguration interp_conf;
-  interp_conf.set("type", "unstructured-bilinear-lonlat");
-  interp_conf.set("non_linear", "missing-if-all-missing-real32");
-  eckit::LocalConfiguration interpolator_conf;
-  interpolator_conf.set("atlas-interpolator", interp_conf);
+    std::vector<eckit::LocalConfiguration> nemo_var_mappings(4);
+    nemo_var_mappings[0].set("name", "sea_ice_area_fraction")
+      .set("nemo field name", "iiceconc")
+      .set("model space", "surface");
+    nemo_var_mappings[1].set("name", "sea_ice_area_fraction_error")
+      .set("nemo field name", "sic_tot_var")
+      .set("model space", "surface")
+      .set("variable type", "background variance");
+    nemo_var_mappings[2].set("name", "sea_surface_foundation_temperature")
+      .set("nemo field name", "votemper")
+      .set("model space", "surface");
+    nemo_var_mappings[3].set("name", "sea_water_potential_temperature")
+      .set("nemo field name", "votemper")
+      .set("model space", "volume");
+    settings_map["ORCA2_T"].geometry_config.set("nemo variables", nemo_var_mappings);
+    eckit::LocalConfiguration interp_conf;
+    interp_conf.set("type", "unstructured-bilinear-lonlat");
+    interp_conf.set("non_linear", "missing-if-all-missing-real32");
+    settings_map["ORCA2_T"].interpolator_config.set("atlas-interpolator", interp_conf);
 
-  OrcaInterpolatorParameters params;
-  params.validateAndDeserialize(interpolator_conf);
+    std::vector<std::string> state_variables {
+        "sea_ice_area_fraction",
+        "sea_surface_foundation_temperature",
+        "sea_water_potential_temperature"};
+    settings_map["ORCA2_T"].state_config.set("state variables", state_variables);
+    settings_map["ORCA2_T"].state_config.set("date", "2021-06-30T00:00:00Z");
+    settings_map["ORCA2_T"].state_config.set("nemo field file", "../Data/orca2_t_nemo.nc");
+    settings_map["ORCA2_T"].state_config.set("variance field file", "../Data/orca2_t_bkg_var.nc");
 
-  std::vector<double> lons({0, 120, 270});
-  std::vector<double> lats({88, 0, 30});
-  const int nlocs = 3;
+    settings_map["ORCA2_T"].lons = std::vector<double>{0, 120, 270};
+    settings_map["ORCA2_T"].lats = std::vector<double>{88, 0, 30};
 
-  // create a state from the test data
-  eckit::LocalConfiguration state_config;
-  std::vector<std::string> state_variables {
-      "sea_ice_area_fraction",
-      "sea_surface_foundation_temperature",
-      "sea_water_potential_temperature"};
-  state_config.set("state variables", state_variables);
-  state_config.set("date", "2021-06-30T00:00:00Z");
-  state_config.set("nemo field file", "../Data/orca2_t_nemo.nc");
-  state_config.set("variance field file", "../Data/orca2_t_bkg_var.nc");
-  OrcaStateParameters stateParams;
-  stateParams.validateAndDeserialize(state_config);
-  State state(geometry, stateParams);
+    settings_map["ORCA2_T"].surf_vars = oops::Variables{{
+        oops::Variable{"sea_ice_area_fraction"},
+        oops::Variable{"sea_surface_foundation_temperature"}}};
+    settings_map["ORCA2_T"].surf_values = std::vector<double>{
+        1,             missing_value, 0,
+        18.4888916016, missing_value, 18.1592999503};
 
-  SECTION("test interpolator succeeds even with no locations") {
-    Interpolator interpolator(interpolator_conf, geometry, {}, {});
+    settings_map["ORCA2_T"].vol_vars = oops::Variables{
+        {oops::Variable{"sea_water_potential_temperature"}}};
+    settings_map["ORCA2_T"].vol_values = std::vector<double>{
+        18.4888916016, missing_value, 18.1592999503,
+        17.9419364929, missing_value, 17.75000288,
+        missing_value, missing_value, missing_value};
   }
 
-  Interpolator interpolator(interpolator_conf, geometry, lats, lons);
+  // AMM1 settings
+  {
+    settings_map["AMM1"].nlocs = 3;
+    settings_map["AMM1"].nlevs = 3;
+    settings_map["AMM1"].geometry_config.set("grid name", "../Data/amm1_atlas_grid_spec.yaml");
+    settings_map["AMM1"].geometry_config.set("number levels", settings_map["AMM1"].nlevs);
 
-  SECTION("test interpolator.apply fails missing variable") {
-    oops::Variables variables{{oops::Variable{"NOTAVARIABLE"}}};
-    std::vector<double> vals(3);
-    std::vector<bool> mask(3, true);
-    EXPECT_THROWS_AS(interpolator.apply(variables, state, mask, vals),
-        eckit::BadParameter);
+    std::vector<eckit::LocalConfiguration> nemo_var_mappings(3);
+    nemo_var_mappings[0].set("name", "sea_surface_height_anomaly")
+      .set("nemo field name", "sossheig")
+      .set("model space", "surface");
+    nemo_var_mappings[1].set("name", "mass_concentration_of_chlorophyll_in_sea_water")
+      .set("nemo field name", "CHL")
+      .set("model space", "surface");
+    nemo_var_mappings[2].set("name", "sea_water_potential_temperature")
+      .set("nemo field name", "votemper")
+      .set("model space", "volume");
+    settings_map["AMM1"].geometry_config.set("nemo variables", nemo_var_mappings);
+    eckit::LocalConfiguration interp_conf;
+    interp_conf.set("type", "unstructured-bilinear-lonlat");
+    interp_conf.set("non_linear", "missing-if-all-missing-real32");
+    settings_map["AMM1"].interpolator_config.set("atlas-interpolator", interp_conf);
+
+    std::vector<std::string> state_variables {
+        "sea_surface_height_anomaly",
+        "mass_concentration_of_chlorophyll_in_sea_water",
+        "sea_water_potential_temperature"};
+    settings_map["AMM1"].state_config.set("state variables", state_variables);
+    settings_map["AMM1"].state_config.set("date", "2021-06-30T00:00:00Z");
+    settings_map["AMM1"].state_config.set("nemo field file", "../Data/amm1_nemo.nc");
+    settings_map["AMM1"].state_config.set("variance field file", "../Data/amm1_nemo.nc");
+
+    settings_map["AMM1"].lons = std::vector<double>{-17.5, -6.78, -16.1};
+    settings_map["AMM1"].lats = std::vector<double>{58.16, 58.91, 63.55};
+
+    settings_map["AMM1"].surf_vars = oops::Variables{{
+        oops::Variable{"sea_surface_height_anomaly"},
+        oops::Variable{"mass_concentration_of_chlorophyll_in_sea_water"}}};
+    settings_map["AMM1"].surf_values = std::vector<double>{
+        -0.470139563084, -0.286416769028, -0.433749824762,
+        0.345775008202,  -236.421875,      2.09327220917};
+
+    settings_map["AMM1"].vol_vars = oops::Variables{
+        {oops::Variable{"sea_water_potential_temperature"}}};
+    settings_map["AMM1"].vol_values = std::vector<double>{
+        11.9501609802, 13.9884538651, 10.1916904449,
+        11.9500904083, 13.8567619324, 10.1912517548,
+        11.9499549866, 13.7289009094, 10.1908035278};
   }
 
-  SECTION("test interpolator.apply") {
-    // two variables at n locations
-    std::vector<double> vals(2*nlocs);
-    std::vector<bool> mask(nlocs, true);
-    interpolator.apply(oops::Variables{{oops::Variable{"sea_ice_area_fraction"},
-        oops::Variable{"sea_surface_foundation_temperature"}}}, state, mask, vals);
+  for (const auto& [key, settings] : settings_map) {
+    Geometry geometry(settings.geometry_config, eckit::mpi::comm());
 
-    double missing_value = util::missingValue<double>();
-    std::vector<double> testvals = {1, missing_value, 0, 18.4888916016,
-                                    missing_value, 18.1592999503};
+    OrcaInterpolatorParameters params;
+    params.validateAndDeserialize(settings.interpolator_config);
 
-    for (size_t i=0; i < testvals.size(); ++i) {
-      std::cout << "vals[" << i << "] " << std::setprecision(12) << vals[i]
-                << " testvals[" << i << "] " << testvals[i] << std::endl;
-      EXPECT(std::abs(vals[i] - testvals[i]) < ATOL);
+    // create a state from the test data
+    OrcaStateParameters stateParams;
+    stateParams.validateAndDeserialize(settings.state_config);
+    State state(geometry, stateParams);
+
+    SECTION("test " + key + " interpolator succeeds even with no locations") {
+      Interpolator interpolator(settings.interpolator_config, geometry, {}, {});
     }
-  }
-  SECTION("test interpolator.apply multiple levels") {
-    std::vector<double> vals(nlevs*nlocs);
-    std::vector<bool> mask(nlocs, true);
-    interpolator.apply(oops::Variables{{oops::Variable{"sea_water_potential_temperature"}}},
-                                       state, mask, vals);
 
-    double missing_value = util::missingValue<double>();
-    std::vector<double> testvals = {
-      18.4888916016, missing_value, 18.1592999503,
-      17.9419364929, missing_value, 17.75000288,
-      missing_value, missing_value, missing_value};
+    Interpolator interpolator(settings.interpolator_config, geometry, settings.lats, settings.lons);
 
-    for (size_t i=0; i < testvals.size(); ++i) {
-      std::cout << "vals[" << i << "] " << std::setprecision(12) << vals[i]
-                << " testvals[" << i << "] " << testvals[i] << std::endl;
-      EXPECT(std::abs(vals[i] - testvals[i]) < ATOL);
+    SECTION("test " + key + " interpolator.apply fails missing variable") {
+      oops::Variables variables{{oops::Variable{"NOTAVARIABLE"}}};
+      std::vector<double> vals(3);
+      std::vector<bool> mask(3, true);
+      EXPECT_THROWS_AS(interpolator.apply(variables, state, mask, vals),
+          eckit::BadParameter);
+    }
+
+    SECTION("test " + key + " interpolator.apply") {
+      // two variables at n locations
+      std::vector<double> vals(2*settings.nlocs);
+      std::vector<bool> mask(settings.nlocs, true);
+      interpolator.apply(settings.surf_vars, state, mask, vals);
+
+      for (size_t i=0; i < settings.surf_values.size(); ++i) {
+        std::cout << "vals[" << i << "] " << std::setprecision(12) << vals[i]
+                  << " kgo_values[" << i << "] " << settings.surf_values[i] << std::endl;
+      }
+      for (size_t i=0; i < settings.surf_values.size(); ++i) {
+        EXPECT(std::abs(vals[i] - settings.surf_values[i]) < ATOL);
+      }
+    }
+    SECTION("test " + key + " interpolator.apply multiple levels") {
+      std::vector<double> vals(settings.nlevs*settings.nlocs);
+      std::vector<bool> mask(settings.nlocs, true);
+      interpolator.apply(settings.vol_vars, state, mask, vals);
+
+      for (size_t i=0; i < settings.vol_values.size(); ++i) {
+        std::cout << "vals[" << i << "] " << std::setprecision(12) << vals[i]
+                  << " kgo_values[" << i << "] " << settings.vol_values[i] << std::endl;
+      }
+      for (size_t i=0; i < settings.vol_values.size(); ++i) {
+        EXPECT(std::abs(vals[i] - settings.vol_values[i]) < ATOL);
+      }
     }
   }
 }
