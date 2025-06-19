925ba94038487f691577a0863b63d9c83acc4e3d
twsearle <14909402+twsearle@users.noreply.github.com>
2025-06-19 11:01:38 +0100

    Updates to versions to match mo-bundle/containers

--- ci/hpccm_recipe_almalinux9.py
+++ ci/hpccm_recipe_almalinux9.py
@@ -22,23 +22,23 @@ def gitlab_url(repo, vn):
 # get versions via --userarg options
 # build hdf5/netcdf with zstd (libzstd from epel)
 atlas_orca_vn = USERARG.get('atlas_orca_vn', '0.4.0')
-atlas_vn = USERARG.get('atlas_vn', '0.38.1')
+atlas_vn = USERARG.get('atlas_vn', '0.40.0')
 blitz_vn = USERARG.get('blitz_vn', '1.0.2')
 boost_vn = USERARG.get('boost_vn', '1.85.0')
-bufr_query_vn = USERARG.get('bufr_query_vn', 'v0.0.1')
-cmake_vn = USERARG.get('cmake_vn', '3.30.0')
-ecbuild_vn = USERARG.get('ecbuild_vn', '3.8.5')
+bufr_query_vn = USERARG.get('bufr_query_vn', '0.0.4')
+cmake_vn = USERARG.get('cmake_vn', '3.26.5')
+ecbuild_vn = USERARG.get('ecbuild_vn', '3.9.0')
 eccodes_vn = USERARG.get('eccodes_vn', '2.30.1')  # requires AEC (libaec-devel) by default
-eckit_vn = USERARG.get('eckit_vn', '1.26.2')
+eckit_vn = USERARG.get('eckit_vn', '1.28.3')
 ectrans_vn = USERARG.get('ectrans_vn', '1.2.0')
-fckit_vn = USERARG.get('fckit_vn', '0.13.0')
+fckit_vn = USERARG.get('fckit_vn', '0.13.3')
 fiat_vn = USERARG.get('fiat_vn', '1.4.1')
-fparser_vn = USERARG.get('fparser_vn', '0.1.4')
-gsl_lite_vn = USERARG.get('gsl_lite_vn', '0.41.0')
-gsw_fortran_vn = USERARG.get('gsw_fortran_vn', '3.07')
-hdf5_vn = USERARG.get('hdf5_vn', 'hdf5-1_14_2')
+fparser_vn = USERARG.get('fparser_vn', '0.2.0')
+gsl_lite_vn = USERARG.get('gsl_lite_vn', '0.42.0')
+gsw_fortran_vn = USERARG.get('gsw_fortran_vn', '3.08')
+hdf5_vn = USERARG.get('hdf5_vn', '1.14.2')
 json_schema_validator_vn = USERARG.get('json_schema_validator_vn', '2.3.0')
-json_vn = USERARG.get('json_vn', '3.9.1')
+json_vn = USERARG.get('json_vn', '3.11.3')
 lapack_vn = USERARG.get('lapack_vn', '3.11.0')
 nccmp_vn = USERARG.get('nccmp_vn', '1.9.1.0')
 nceplibs_bufr_vn = USERARG.get('nceplibs_bufr_vn', '12.0.1')
@@ -61,9 +61,7 @@ COMMON_PACKAGES = [
     'eigen3-devel',
     'expat-devel',
     'flex',
-    'gcc',
-    'gcc-c++',
-    'gcc-gfortran',
+    'gcc-toolset-12',
     'git',
     'git-lfs',
     'gmp-devel',
@@ -90,6 +88,7 @@ COMMON_PACKAGES = [
     'python3-pip',
     'python3-pytest',
     'python3-pyyaml',
+    'python3-scipy',
     'rsync',
     'time',
     'unzip',
@@ -106,6 +105,13 @@ Stage0 += shell(commands=[
     'dnf config-manager -y --set-enabled crb',
 ])
 Stage0 += packages(epel=True, ospackages=COMMON_PACKAGES)
+
+Stage0 += environment(variables={
+    'BASH_ENV': '/opt/rh/gcc-toolset-12/enable',
+    'ENV': '/opt/rh/gcc-toolset-12/enable',
+    'PATH': '/opt/rh/gcc-toolset-12/root/bin:$PATH',
+    'LD_LIBRARY_PATH': '/opt/rh/gcc-toolset-12/root/lib:/opt/rh/gcc-toolset-12/root/lib64',
+})
 
 Stage0 += cmake(eula=True, version=cmake_vn)
 
@@ -134,13 +140,11 @@ mpi = openmpi(
 )
 Stage0 += mpi
 
-Stage0 += generic_autotools(
+Stage0 += hdf5(
+    version=hdf5_vn,
     prefix='/usr/local',
-    repository='https://github.com/HDFGroup/hdf5',
-    commit=hdf5_vn,
+    configure_opts=['--enable-cxx', '--enable-fortran'],
     enable_build_mode='production',
-    enable_cxx=True,
-    enable_fortran=True,
     enable_parallel=True,
     enable_threadsafe=True,
     enable_unsupported=True,
@@ -152,7 +156,7 @@ Stage0 += generic_autotools(
 # Nonexistent include directory '/var/tmp/hdf5-1.14.0/src/H5FDsubfiling'
 # sed -i "s|-I${TMPDIR}/hdf5-${hdf5_vn}/src/H5FDsubfiling||g" "/usr/local/bin/h5pcc"
 Stage0 += shell(commands=[
-    f'sed -i "s|-I/var/tmp/hdf5/src/H5FDsubfiling||g" "/usr/local/bin/h5pcc"'
+    f'sed -i "s|-I/var/tmp/hdf5-${hdf5_vn}/src/H5FDsubfiling||g" "/usr/local/bin/h5pcc"'
 ])
 Stage0 += environment(variables={'H5DIR': '/usr/local', 'LIBS': '-ldl'})
 Stage0 += netcdf(
@@ -252,8 +256,8 @@ for org, name, vn in (
 # -- bufr-query
 Stage0 += generic_cmake(
     prefix='/usr/local',
-    repository='https://github.com/NOAA-EMC/bufr-query',
-    commit=bufr_query_vn,
+    url=github_url('NOAA-EMC/bufr-query', f'v{bufr_query_vn}'),
+    directory=f'bufr-query-{bufr_query_vn}',
     cmake_opts=['-DCMAKE_BUILD_TYPE=Release', '-DMPI=ON', '-DOMP=ON'],
     build_environment={'LDFLAGS': '-lnetcdf'},
 )
@@ -292,11 +296,16 @@ Stage1 += shell(commands=[
     'dnf config-manager -y --set-enabled crb',
 ])
 Stage1 += packages(epel=True, ospackages=COMMON_PACKAGES)
-Stage1 += pip(pip='pip3', packages=['cpplint', 'yamlprocessor'])
+Stage1 += pip(pip='pip3', packages=[
+    'cpplint',
+])
 Stage1 += copy(_from='build', src='/usr/local', dest='/usr/local')
 Stage1 += shell(commands=['ln -sfT python3 /usr/bin/python'])
 Stage1 += environment(variables={
-    'PATH': '/usr/local/bin:$PATH',
-    'LD_LIBRARY_PATH': '/usr/local/lib64:/usr/local/lib:/usr/lib64:/usr/lib:$LD_LIBRARY_PATH',
+    'BASH_ENV': '/opt/rh/gcc-toolset-12/enable',
+    'ENV': '/opt/rh/gcc-toolset-12/enable',
+    'LD_LIBRARY_PATH': '/usr/local/lib64:/usr/local/lib:/opt/rh/gcc-toolset-12/root/lib:/opt/rh/gcc-toolset-12/root/lib64:/usr/lib64:/usr/lib',
+    'PATH': '/usr/local/bin:/opt/rh/gcc-toolset-12/root/bin:$PATH',
+    'VALIDATE_PARAMETERS': '1',
 })
 Stage1 += workdir(directory='/var/tmp')
