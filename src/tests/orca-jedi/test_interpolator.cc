038772536fbacbd5bb274bae30bc9d602ac6643e
twsearle <14909402+twsearle@users.noreply.github.com>
2026-01-23 16:18:54 +0000

    relax test condition

--- src/tests/orca-jedi/test_interpolator.cc
+++ src/tests/orca-jedi/test_interpolator.cc
@@ -385,7 +385,7 @@ CASE("test  interpolator") {
         // The adjoint test: <y, H*x> should equal <H^T*y, x> within numerical precision,
         // however with the land mask some information is spread over land points and then
         //  zeroed out in the adjoint interpolator.
-        EXPECT(std::abs(dot_HtY_x) < std::abs(dot_y_Hx));
+        EXPECT(dot_HtY_x <= dot_y_Hx);
       }
     }
   }
